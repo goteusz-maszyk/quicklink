881a4854ae0e14c649025ce7e458bc54c512f3a1
goteusz-maszyk <mateusz.goszyk@gmail.com>
2024-01-19 20:49:07 +0100

    account update panel + *security fixes*

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ app.set('view engine', 'ejs');
 app.use(session({
   secret: randomUUID(),
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  cookie: {
+    sameSite: "strict"
+  }
 }))
 
 app.use((req, res, next) => {
@@ -29,14 +32,26 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  if (req.session.user != null) {
+  if (req.session.user != null && auth(req.session.user.username, req.session.user.encrypted_password, true)) {
     res.redirect("/admin/dashboard")
     res.end()
     return
   }
 
   res.render("index")
-})
+});
+
+app.post('/', async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  const user = await auth(username, password, false);
+  if (user != null) {
+    req.session.user = user;
+  }
+  res.redirect("/");
+  res.end();
+});
 
 app.get('/admin/dashboard', async (req, res) => {
   const links = await prisma.link.findMany()
@@ -44,34 +59,19 @@ app.get('/admin/dashboard', async (req, res) => {
 });
 
 app.post('/admin/createlink', async (req, res) => {
-  if (!req.body.name || !req.body.shortLinkName || !req.body.url) {
-    res.redirect("/admin/dashboard")
-    res.end()
-    return
+  if (req.body.name && req.body.shortLinkName && req.body.url && !(await prisma.link.findFirst({ where: { short_id: req.body.shortLinkName }}))) {
+    await prisma.link.create({
+      data: {
+        name: req.body.name,
+        short_id: req.body.shortLinkName,
+        url: req.body.url
+      }
+    })
   }
-  await prisma.link.create({
-    data: {
-      name: req.body.name,
-      short_id: req.body.shortLinkName,
-      url: req.body.url
-    }
-  })
 
   res.redirect("/admin/dashboard")
   res.end()
 })
-
-app.post('/', async (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-
-  const user = await auth(username, password);
-  if (user != null) {
-    req.session.user = user;
-  }
-  res.redirect("/");
-  res.end();
-});
 
 app.get('/admin/logout', (req, res) => {
   req.session.destroy((err) => {
@@ -84,13 +84,54 @@ app.get('/admin/logout', (req, res) => {
 });
 
 app.get('/admin/delete/:id', async (req, res) => {
-  await prisma.link.delete({
-    where: {
-      short_id: req.params.id
-    }
-  })
-
+  try {
+    await prisma.link.delete({
+      where: {
+        short_id: req.params.id
+      }
+    })
+  } catch (e) {}
   res.redirect("/dashboard")
+  res.end()
+});
+
+app.get('/admin/account', async (req, res) => {
+  const { username } = req.session.user;
+  res.render("account", { username });
+  res.end();
+});
+
+app.post('/admin/changename', async (req, res) => {
+  const username = req.body.name
+  const user = await prisma.user.update({
+    where: {
+      id: req.session.user.id
+    },
+    data: {
+      username: username
+    }
+  });
+  req.session.user = user
+
+  res.redirect('/admin/account')
+  res.end()
+})
+
+app.post("/admin/changepassword", async (req, res) => {
+  const { current_password, new_password } = req.body;
+
+  if (bcrypt.compareSync(current_password, req.session.user.encrypted_password)) {
+    await prisma.user.update({
+      where: {
+        id: req.session.user.id
+      },
+      data: {
+        encrypted_password: bcrypt.hashSync(new_password, 10)
+      }
+    });
+  }
+
+  res.redirect('/admin/logout')
   res.end()
 })
 
@@ -114,7 +155,7 @@ app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
 
-async function auth(username, password) {
+async function auth(username, password, encrypted) {
   const user = await prisma.user.findFirst({
     where: {
       username: username
@@ -123,9 +164,17 @@ async function auth(username, password) {
   if (user == null) {
     return null
   }
-  return bcrypt.compareSync(password, user.encrypted_password) ? user : null
+  if (encrypted) {
+    return user.encrypted_password == password ? user : null
+  } else {
+    return bcrypt.compareSync(password, user.encrypted_password) ? user : null
+  }
 }
 
 process.on("exit", async (_code) => {
   await prisma.$disconnect()
+})
+
+process.on('uncaughtException', (e) => {
+  console.error(e)
 })
